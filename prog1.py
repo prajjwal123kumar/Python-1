9c625889739bba0f8e5041c74f31d97cf059c8aa
prajjwal123kumar <94749894+prajjwal123kumar@users.noreply.github.com>
2022-02-18 07:28:07 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,19 @@
+x=10
+y=2
+z=x+y
+print("Hello")
+print("Hello","Hi")
+print(x,y,z)
+
+a=type("Hello")
+print(a)
+print(type(a))
+x=5
+print(type(x))
+
+# z=print(type(x))
+# print(z)
+
+
+x=input("Enter number: ")
+print(x,type(x))
