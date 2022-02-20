c8c010cb6edaa72adecfd0c80c1d8847584b5903
prajjwal123kumar <94749894+prajjwal123kumar@users.noreply.github.com>
2022-02-19 22:20:46 -0500

    Add files via upload

---
+++ oddevenProg.py
@@ -0,0 +1,14 @@
+# WAP to check no is even or odd
+
+#BLL code start from here
+def oddEven(x):
+    if x%2==0:
+        print(x,"is even number.")
+    else:
+        print(x,"is odd number.")
+#BLL code ends here
+
+#PL code start from here
+x=int(input("Enter a Number : "))
+oddEven(x)
+#PL code ends here
