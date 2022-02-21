ae0a5770aedd656c4d0555d09628622bd82ece6f
prajjwal123kumar <94749894+prajjwal123kumar@users.noreply.github.com>
2022-02-21 12:39:26 -0500

    Add files via upload

---
+++ whileProg4.py
@@ -0,0 +1,16 @@
+# WAP to print all characters of x
+
+x="Hello"
+i=0
+while(i<len(x)):
+    y=x[i]
+    print(y)
+    i=i+1
+
+# OR
+
+x="Hello"
+i=0
+while(i<len(x)):
+    print(i,"=>",x[i])
+    i=i+1
